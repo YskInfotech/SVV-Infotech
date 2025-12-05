215574b59a7e133444f98a7e41e3d614f1057b12
Revanthsvv <revanthkumar@svvinfotech.in>
2025-12-05 18:48:19 +0530

    remove duplicates

--- svv-infotech/src/components/AdminDashboard/DashboardRoutes.js
+++ svv-infotech/src/components/AdminDashboard/DashboardRoutes.js
@@ -10,18 +10,12 @@ import Shortlistcandidates from "../Pages/Shortlistcandidates";
 import Rejectcandidates from "../Pages/Rejectcandidates";
 import Pendingcandidates from "../Pages/Pendingcandidates";
 import Candidateview from "../Pages/Candidateview";
-<<<<<<< HEAD
 import Onboardingview from "../adminlogin/onboarding/Onboardingview";
 import Onboardlanding from "../adminlogin/onboarding/Onboardlanding";
 
-=======
-import Documents_ID from "../Pages/Documents_ID";
-import Nominee_Bankdetails from "../Pages/Nominee_Bankdetails";
-import JoiningDoc from "../Pages/JoiningDoc";
-import Onboardingview from "../adminlogin/onboarding/Onboardingview";
-import Onboardinginformation from "../adminlogin/onboarding/Oboardinginformation";
-import Pesonalinformation from "../Pages/Pesonalinformation";
->>>>>>> 5972d1910de51ed101db50f04342ebf9e6b68475
+
+
+
 
 
 function DashboardRoutes() {
@@ -37,13 +31,6 @@ function DashboardRoutes() {
        <Route path="/Rejectcandidates" element={<Rejectcandidates/>}/>
        <Route path="/Pendingcandidates" element={<Pendingcandidates/>}/>
        <Route path="/Candidateview" element={<Candidateview/>}/> 
-<<<<<<< HEAD
-=======
-       <Route path="/pesonalinformation" element={<Pesonalinformation/>}/> 
-       <Route path="/Documents_id" element={<Documents_ID/>}/>
-       <Route path="/Nominee_bankdetails" element={<Nominee_Bankdetails/>}/>
-       <Route path="/JoiningDoc" element={<JoiningDoc/>}/>
->>>>>>> 5972d1910de51ed101db50f04342ebf9e6b68475
        <Route path="/Onboardingview" element={<Onboardingview/>}/>
        <Route path="/Onboardinglanding/*" element={<Onboardlanding/>}/>
        
